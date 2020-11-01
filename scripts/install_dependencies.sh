310dacebe660e58467cc8d8a7860eea19ea75872
Henry Dashwood <henry.dashwood@curationcorp.com>
2020-11-01 23:34:09 +0000

    hakyll

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install -y apache2
+sudo apt install -y apache2 
+sudo apt-get install -y haskell-stack
+stack install hakyll
 
 if [ -f /var/www/html/index.html ]
 then
